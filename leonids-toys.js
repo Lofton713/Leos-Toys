dd86f567c0a9382d24c0b00dee26c9bc3966f4af
stacy lofton <lofton713@gmail.com>
2023-05-25 10:39:16 -0500

    initial commit

---
+++ leonids-toys.js
@@ -0,0 +1,52 @@
+const toys = [
+
+{
+    id: 1,
+    name:"RC_Car",
+    maker:"Tyco",
+    price:45,
+    weight: 7
+},
+{
+    id: 2,
+    name: 'Gameboy_DS',
+    maker: 'Nintendo',
+    price: 120,
+    weight: 2
+},
+{
+    id: 3,
+    name: 'Home_ Alone_House',
+    maker: 'Lego',
+    price: 150,
+    weight: 7
+}
+]
+
+const xbox = {
+    id: 4,
+    name: 'xbox',
+    maker: 'Microsoft',
+    price: 250,
+    weight: 7 
+}
+
+const soccerBall = {
+    id: 5,
+    name: 'Tango',
+    maker: 'Adidas',
+    price: 150,
+    weight: 1
+}
+toys.push(xbox, soccerBall)
+
+
+for (const toy of toys) {
+    //Return only one toy based on id
+    if (toy.id === toyToFind){
+    // increase the cost of each toy by 5%
+    toy.price = toy.price * 1.05
+    console.log( `the ${toy.maker} ${toy.name} costs $${toy.price}.`)
+
+    }
+}
